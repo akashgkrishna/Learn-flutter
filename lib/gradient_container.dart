47f14e1f4ccd8fb9fa9c69fa21b424f3029e86ff
akashkrishna7 <akashkg@testvagrant.com>
2024-05-03 14:16:40 +0530

    Added variable to accept value dynamically

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/text_style.dart';
 
+var startColor = Alignment.topLeft;
+var endColor = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration:  BoxDecoration(
+        gradient: LinearGradient(colors: const[
           Colors.cyan,
           Color.fromARGB(255, 67, 25, 184),
           Colors.cyan,
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ], begin: startColor, end: endColor),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText("Hello!! Welcome"),
       ),
     );
   }
 }
-
